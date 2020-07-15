bfa58667f0cf1f1f5bc793f209b1f82896571532
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-07-15 11:27:03 +0300

    form include

--- form.php
+++ form.php
@@ -67,21 +67,8 @@ function is_natural_check($str)
 {
 	return ctype_digit((string) $str);
 }
-?>
 
-<h1>KMI Skaičiuoklė</h1>
-
-<div class="container">
-    <form action="http://localhost:8888/kmi/" method="post"> 
-        <label for="ugis">Ūgis: <span class="text">(nuo 1.50 iki 1.99)</span></label><br>
-        <input type="text" name="ugis" placeholder="0.00" value="<?php echo $ugis;?>" autocomplete="off"><br>
-        <label for="svoris">Svoris: <span class="text">(nuo 40 iki 99)</span></label><br>
-        <input type="text" name="svoris" value="<?php echo $svoris;?>" autocomplete="off"><br>
-        <input type="submit" value="Skaičiuoti">
-    </form>
-</div>
-
-<?php
+include_once('form1.php');
 
 if ( !empty($ugis) AND !empty($svoris)) {  
     validation($svoris, $ugis);
@@ -89,6 +76,10 @@ if ( !empty($ugis) AND !empty($svoris)) {
 } else {
     if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
     echo  '<div class="error error-bg">Užpildykite laukelius !</div>';
+    } else {
+        $ugis = FALSE;
+        $svoris = FALSE;
+        include_once('form1.php');
     }
 }
 ?>
